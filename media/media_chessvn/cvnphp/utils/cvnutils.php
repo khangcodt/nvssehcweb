4b7e26ad829d52dbd63cc417b7deb9cc80d93bef
khanglq2011 <khanglq2011@gmail.com>
2015-03-25 08:42:32 +0000

    add PHP doc for function genRandomString

--- media/media_chessvn/cvnphp/utils/cvnutils.php
+++ media/media_chessvn/cvnphp/utils/cvnutils.php
@@ -1,4 +1,9 @@
 <?php
+
+/**
+ * @param int $length: Length of random String
+ * @return string: Random string of specific length, default = 15
+ */
 function genRandomString($length = 15)
 {
     return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
