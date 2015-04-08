76773d88623b2e735c49be7cd311847579afe713
khanglq2011@gmail.com <khanglq2011@gmail.com@98a817a5-4496-c9fd-2fea-ae7df56dd1dc>
2015-04-08 10:18:56 +0000

    Draft of module challenges & sql for top players

--- trunk/mod_viewchallenges/tmpl/default.php
+++ trunk/mod_viewchallenges/tmpl/default.php
@@ -9,44 +9,47 @@
 // no direct access
 defined('_JEXEC') or die;
 $document = JFactory::getDocument();
-$mediaPath = JPATH_ROOT . '/media/media_chessvn/';
+$mediaPath = JURI::base() . '/media/media_chessvn/';
 $document->addScript($mediaPath.'js/sorttable.js');
+$document->addScript($mediaPath.'js/jquery/jquery-1.8.2.min.js');
 $document->addStyleSheet($mediaPath.'css/chessvn.css');
+$userid = JFactory::getUser()->id;
 ?>
 
-<?php if ($showmode == 0 || $showmode == 2) : ?>
-	<?php $guest = JText::plural('MOD_ONLINEPLAYERS_GUESTS', $count['guest']); ?>
-	<?php $member = JText::plural('MOD_ONLINEPLAYERS_MEMBERS', $count['user']); ?>
-	<p><?php echo JText::sprintf('MOD_ONLINEPLAYERS_WE_HAVE', $guest, $member); ?></p>
-<?php endif; ?>
+<script type="text/javascript">
 
-<?php if (($showmode > 0) && count($names)) : ?>
+    var baseUri = <?php echo JURI::base(); ?>;
+    var userid = <?php echo $userid;?>;
+    var updateChallengesSpeed = 10000;//get from config file
+    function getXmlChallenges(){
+        var url= baseUri + '?option=com_chessvn&format=xml&userid='+userid;
+        console.log('url = ' + url);
+        jQuery.get(url, processXmlChallenges);
+    }
 
-	<?php if ($params->get('filter_groups')):?>
-		<p><?php echo JText::_('MOD_ONLINEPLAYERS_SAME_GROUP_MESSAGE'); ?></p>
-	<?php endif;?>
-        <table class="sortable" width="100%">
-            <thead>
-            <tr>
-                <th><a><?php echo JText::_('MOD_ONLINEPLAYERS_THEAD_PLAYER'); ?></a></th>
-                <th><a><?php echo JText::_('MOD_ONLINEPLAYERS_THEAD_RATING'); ?></a></th>
-                <th><a><?php echo JText::_('MOD_ONLINEPLAYERS_THEAD_COIN'); ?></a></th>
-				<th><a>Live</a></th>
-            </tr>
-            </thead>
-            <tbody>
-	<?php
-    foreach($names as $name) :
-        $live = (in_array($name, $liveUsers))?'ON':'OFF';
-    ?>
-            <tr>
-                <td><?php echo $name->username; ?></td>
-                <td><?php echo $name->ratingpoint; ?></td>
-                <td><?php echo $name->coin; ?></td>
-				<td><?php echo $live; ?></td>
-            </tr>
-	<?php endforeach;  ?>
-            </tbody>
-        </table>
-<?php endif;  ?>
+    function processXmlChallenges(xmlData) {
+        window.setTimeout('getXmlChallenges()', updateChallengesSpeed);
+        var i, games = jQuery(xmlData).find('GAMES'), content='', desc, gid, wid, bid, initid, status, color, timeout, type, fen;
 
+        jQuery('#game_details').remove();
+        opendiv.empty();
+        for(i=0; i<games.length; i++){
+            desc=games[i].getElementsByTagName('DESCRIPTION').item(0).firstChild.data;
+            gid=games[i].getElementsByTagName('GAMEID').item(0).firstChild.data;
+            status=games[i].getElementsByTagName('STATUS').item(0).firstChild.data;
+            initid=games[i].getElementsByTagName('INITIATOR').item(0).firstChild.data;
+            wid=games[i].getElementsByTagName('WHITE').item(0).firstChild.data;
+            bid=games[i].getElementsByTagName('BLACK').item(0).firstChild.data;
+            timeout=games[i].getElementsByTagName('TIMEOUT').item(0).firstChild.data;
+            type=games[i].getElementsByTagName('GAMETYPE').item(0).firstChild.data;
+            rated=games[i].getElementsByTagName('RATED').item(0).firstChild.data;
+            time_created=games[i].getElementsByTagName('TIMECREATED').item(0).firstChild.data;
+            time_ctrl1=games[i].getElementsByTagName('TIMECONTROL1').item(0).firstChild.data;
+            time_ctrl2=games[i].getElementsByTagName('TIMECONTROL2').item(0).firstChild.data;
+            fen = games[i].getElementsByTagName('GAMEFEN').item(0).firstChild.data;
+
+            var data = {desc: desc, gid: gid, status: status, initid: initid, wid: wid, bid: bid, timeout: timeout, type: type, rated: rated, time_created: time_created, time_ctrl1: time_ctrl1, time_ctrl2: time_ctrl2, fen: fen};
+            create_game_field(data, true);
+        }
+    }
+</script>
