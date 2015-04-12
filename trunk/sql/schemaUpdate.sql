f1f70ff7dc346ccd7e8b951020ccfd5965b88202
khanglq2011@gmail.com <khanglq2011@gmail.com@98a817a5-4496-c9fd-2fea-ae7df56dd1dc>
2015-04-12 14:21:20 +0000

    update mod_viewchallenges

--- trunk/sql/schemaUpdate.sql
+++ trunk/sql/schemaUpdate.sql
@@ -1,3 +1,9 @@
+# ===================================================================
+# IMPORTANT: nhớ thay tiền tố tương ứng với db local khi update sql
+# ===================================================================
+
+# 4:01 PM 4/11/2015
+# table lấy top player, jetset theo tuần, tháng
 CREATE TABLE cvn_gameresultplayer
 (
   gameresultplayerid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -9,3 +15,35 @@ CREATE TABLE cvn_gameresultplayer
   coinchange BIGINT(20),
   createdtime DATETIME
 );
+# thêm trường oponentid
+ALTER TABLE cvn_gameoption
+ADD COLUMN `oponentid` INT(11) NULL COMMENT 'đối thủ trong game, null nếu là open game' AFTER `initiator`;
+
+# update viewchallenges, thêm oponentid, oponentname
+CREATE OR REPLACE VIEW cvn_viewchallenges AS
+  SELECT
+    g.userid AS userid,
+    a.gameid AS gameid,
+    a.status AS status,
+    a.completionstatus AS completionstatus,
+    p.gametitle AS gametitle,
+    p.chesstype AS chesstype,
+    p.initiator AS initiator,
+    p.oponentid AS oponentid,
+    u.username AS initiatorname,
+    uo.username AS oponentname,
+    p.gamecoin AS gamecoin,
+    p.elowhite AS elowhite,
+    p.eloblack AS eloblack,
+    p.israting AS israting,
+    p.timemode AS timemode,
+    p.maintime AS maintime,
+    p.incrementtime AS incrementtime,
+    p.createdtime AS createdtime
+  FROM
+    (((((cvn_users u JOIN cvn_player g ON ((g.userid = u.id)))
+  LEFT JOIN cvn_gameoption p ON ((p.initiator = g.playerid)))
+  JOIN cvn_game a ON ((a.gameid = p.gameid)))
+  LEFT JOIN cvn_player po ON ((p.oponentid = po.playerid)))
+  LEFT JOIN cvn_users uo ON ((po.userid = uo.id)));
+# ===============================================================
