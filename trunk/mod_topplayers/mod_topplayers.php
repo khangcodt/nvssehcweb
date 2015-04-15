40550ca46e3c120c2124dac2361a968d4873c886
khanglq2011@gmail.com <khanglq2011@gmail.com@98a817a5-4496-c9fd-2fea-ae7df56dd1dc>
2015-04-15 10:00:27 +0000

    update code for topplayer by week, month, Duy check code nhé

--- trunk/mod_topplayers/mod_topplayers.php
+++ trunk/mod_topplayers/mod_topplayers.php
@@ -12,7 +12,11 @@ defined('_JEXEC') or die;
 // Include the whosonline functions only once
 require_once dirname(__FILE__).'/helper.php';
 
-$showmode = $params->get('showmode', 0);
+//$showmode = $params->get('showmode', 0);
+$toptypeconfig = $params->get('topplayertype', "all"); //get default type from config of module
+$jinput = JFactory::getApplication()->input;
+$topplayertype = $jinput->get('topplayertype', $toptypeconfig);// get request param of topplayertype, default from config
+//thêm tham số $topplayertype vào hàm getTopPlayers($params, $topplayertype = 'all'), tùy vào tham số này thì query ở các view tương ứng
 
 	$count	= modTopplayersHelper::getTopCount();
 
