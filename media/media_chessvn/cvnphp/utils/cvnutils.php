670f620f4dcb4951a0002b82e5d4040679e05eaa
khanglq2011 <khanglq2011@gmail.com>
2015-04-21 15:57:22 +0000

    update nguyên mẫu hàm dùng chung isEmailUpdated

--- media/media_chessvn/cvnphp/utils/cvnutils.php
+++ media/media_chessvn/cvnphp/utils/cvnutils.php
@@ -9,4 +9,15 @@ function genRandomString($length = 15)
     return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
 }
 
+
+/**
+ * return true if inputEmail match with prefix, suffix and length of random middle string, otherwise false
+ * @param $inputEMail
+ * @param $emailPrefix
+ * @param $emailSuffix
+ * @param $randomMidLength
+ */
+function isEmailUpdated($inputEMail, $emailPrefix, $emailSuffix, $randomMidLength) {
+    //tìm hiểu cách sử dụng regular expression để code
+}
 ?>
