c8ef1769e60216413f4e80e94e09e7ddcfe55017
khanglq2011@gmail.com <khanglq2011@gmail.com@98a817a5-4496-c9fd-2fea-ae7df56dd1dc>
2015-05-16 03:02:07 +0000

    xoa folder commit loi

--- branches/branch_fbsdk4/admin/views/test/tmpl/default_raw.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/**
-* @package 		Facebook Connect Extension (joomla 1.5.x & 1.6.x)
-* @copyright	Copyright (C) Computer - http://www.saaraan.com. All rights reserved.
-* @license		http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-* @author		Saran Chamling
-* @download URL	http://www.saaraan.com
-*/
-?>
