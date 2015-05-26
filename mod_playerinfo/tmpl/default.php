1030b3b1c24c35e9ea480854edc65ce33e67f0d3
Nguyensonghao974 <Nguyensonghao974@gmail.com>
2015-05-26 12:17:24 +0000

    update mod_playerinfo

--- mod_playerinfo/tmpl/default.php
+++ mod_playerinfo/tmpl/default.php
@@ -1,21 +1,21 @@
 <?php
 $document = JFactory::getDocument();
 $mediaPath = JURI::base() . '/media/media_chessvn/';
-$document->addStyleSheet($mediaPath.'css/inforuser.css');
+$document->addStyleSheet($mediaPath.'css/chessvn.css');
 // Thêm file css
 ?>
-    <div id="information">
+    <div id="mod-playerinfo">
         <?php
             foreach($result as $key=>$value){
         ?>
-                <img src="<?php echo JURI::base() .'/media_chessvn'.$value->mediaplayer.$value->avatar ?>" height="100px" width="100px">
+                <img src="<?php echo $mediaPath.$value->mediaplayer.$value->avatar ?>" height="100px" width="100px">
                 <div id="player_infor">
                     <p><?php echo JText::_('MOD_PLAYERINFO_INFOR_LABEL') ?></p>
                     <p><?php echo JText::_('MOD_PLAYERINFO_NAME_LABEL').$value->username?></p>
                     <p><?php echo JText::_('MOD_PLAYERINFO_RATING_LABEL').$value->ratingpoint ?></p>
                     <p><?php echo JText::_('MOD_PLAYERINFO_COIN_LABEL').$value->coin ?></p>
                 </div>
-                <div id="game_infor">
+                <div id="mod-playerinfo-game">
                     <p><?php echo JText::_('MOD_PLAYERINFO_GAMEINFOR_LABEL') ?></p>
                     <p><?php echo JText::_('MOD_PLAYERINFO_GAME_LABEL').($value->gamewin+$value->gamelost+$value->gamedraw) ?></p>
                     <p><?php echo JText::_('MOD_PLAYERINFO_WIN_LABEL').$value->gamewin ?><p>
