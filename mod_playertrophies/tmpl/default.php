1594608e1587071ab534db76a625b3a9b211acf0
hoang93hn <hoang93hn@gmail.com>
2015-06-23 07:39:47 +0000

    Update default mod_playertrophies

--- mod_playertrophies/tmpl/default.php
+++ mod_playertrophies/tmpl/default.php
@@ -9,24 +9,13 @@ $document->addStyleSheet($mediaPath . 'css/chessvn.css');
     <table>
         <?php
         foreach ($trophies as $trophy):
-            $jText = 'acb';
             $trophyImg = '<img width="45px" height="45px" alt="avatar" src="' . $mediaPath . 'images/' . $trophy->imageurl . '" style="margin: 0;">';
             $trophyLabel= $trophy->name;
-            switch($trophyLabel){
-                case  'win3':
-                    $jText ='MOD_PLAYERTROPHIES_WIN3';
-                    break;
-                case 'win5':
-                    $jText ='MOD_PLAYERTROPHIES_WIN5';
-                    break;
-                case 'win10':
-                    $jText ='MOD_PLAYERTROPHIES_WIN10';
-                    break;
-            }
+
             ?>
             <tr>
                 <td><?php echo $trophyImg; ?></td>
-                <td><?php echo JText::_($jText) ?></td>
+                <td><?php echo JText::_($trophyLabel) ?></td>
             </tr>
         <?php endforeach; ?>
     </table>
