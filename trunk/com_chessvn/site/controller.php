cdd06d2a50ea6af68e901ff4218a34d0820d8ce6
Nguyensonghao974@gmail.com <Nguyensonghao974@gmail.com@98a817a5-4496-c9fd-2fea-ae7df56dd1dc>
2015-07-06 15:36:49 +0000

    mod_chat

--- trunk/com_chessvn/site/controller.php
+++ trunk/com_chessvn/site/controller.php
@@ -31,5 +31,32 @@ class ChessvnController extends JControllerLegacy
 		parent::display($cachable, $urlparams);
 	}
 
+    public function send(){
+        $gameid = $_POST['gameid'];
+        $userid = $_POST['userid'];
+        $message = $_POST['message'];
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        // Lấy playerid từ userid
+        $query->select('playerid');
+        $query->from('#__player');
+        $query->where('userid = '.$userid);
+        $db->setQuery($query);
+        $playerid = $db->loadObject()->playerid;
+
+
+        $today = date("Y-m-d, H:i s");
+        $query = $db->getQuery(true);
+        $columns = array('gameid', 'playerid', 'chatmsg', 'createddate');
+        $values = array($db->quote($gameid), $playerid, $db->quote($message), 'now()');
+
+        $query->insert($db->quoteName('#__gamechat'))
+            ->columns($db->quoteName($columns))
+            ->values(implode(',', $values));
+
+        $db->setQuery($query);
+        $db->query();
+    }
+
 }
 ?>
