d55386625f859754a1e65f6a2a64b66828a96bb3
Nguyensonghao974 <Nguyensonghao974@gmail.com>
2015-07-12 09:02:02 +0000

    set timeipdate for player and viewer

--- mod_chat/mod_chat.php
+++ mod_chat/mod_chat.php
@@ -2,5 +2,8 @@
 
 // no direct access
 defined('_JEXEC') or die;
+require_once dirname(__FILE__).'/helper.php';
+$gameid = JRequest::getVar('gameid');
+$result = modChatHelper::CheckplayerID($gameid);
 
-require JModuleHelper::getLayoutPath('mod_chat', $params->get('layout', 'default'));
+require_once dirname(__FILE__).'/helper.php';require JModuleHelper::getLayoutPath('mod_chat', 'default');
